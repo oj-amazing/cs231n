7f82c256c4c807576553ac22cdf469514e026ba6
Olivier Jin <olivier.jin@gmail.com>
2017-05-29 01:44:22 +0000

    Retraining script, rev 1

---
+++ code/oj_run.py
@@ -0,0 +1,37 @@
+import subprocess as sub
+import os
+
+home = os.path.expanduser('~')
+path = home + "/tensorflow/"
+image_dir = home + "/cs231n/dataset_flat"
+
+#Default values
+learning_rate = 0.01
+how_many_training_steps = 4000
+train_batch_size = 100
+random_crop = 0
+random_scale = 0
+random_brightness = 0
+flip_left_right = False
+
+# Epoch calculations
+
+
+
+# Sweep values
+learning_rates = [x / 100.0 for x in range(1,21,2)]
+
+for learning_rate in learning_rates:
+  filename = str(learning_rate) + "_" + \
+    str(how_many_training_steps) + "_" + \
+    str(train_batch_size) + "_" + \
+    str(random_crop) + "_" + \
+    str(random_scale) + "_" + \
+    str(random_brightness) + "_" + \
+    str(flip_left_right) + ".out"
+  
+  outFile = open(filename, 'w')
+  
+  sub.Popen([path + 'bazel-bin/tensorflow/examples/image_retraining/retrain', '--image_dir', image_dir, '--learning_rate', str(learning_rate)], stdout = outFile)
+
+  outFile.close()

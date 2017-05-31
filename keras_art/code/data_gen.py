446ee081f9f8550aaf9a699e590623ca9b5ae9e8
vanillayyy <vanillayyy@gmail.com>
2017-05-31 00:37:33 -0700

    trainvaltest split

--- keras_art/code/data_gen.py
+++ keras_art/code/data_gen.py
@@ -3,21 +3,34 @@ import os
 import numpy as np
 import pickle
 import shutil
+#for small data
 CAT_SIZE = 100
-VAL_SPLIT = 0.2
-BASE_DIR = "data/Pandora18K_small_train_val_split/"
+
+#for full data
+#CAT_SIZE = 10**5 
+
+#for small data
+BASE_DIR = "data/Pandora18K_small_train_val_test_split/"
+#for full data
+# BASE_DIR = "data/Pandora18K_train_val_test_split/"
+
+
+
 
 cmd = "np.array([0]*65+[1]*14+[2]*125)"
 cat_count_train_list = []
 cat_count_val_list = []
 cat_count_total_list = []
+cat_count_test_list = []
+np.random.seed(231)
 if __name__ == "__main__":
     total_count = 0
     for root, dirs, files in os.walk("data/Pandora_18k_flat/"):
         for dir in dirs:
             same_cat_count =0
             same_cat_count_train = 0
-            same_cat_count_val= 0
+            same_cat_count_val = 0
+            same_cat_count_test = 0
             print(dir)
             for root_f, _, files in os.walk(os.path.join(root,dir)):
                 for file in files:
@@ -28,12 +41,15 @@ if __name__ == "__main__":
                             cur_path = os.path.join(root_f,file)
 
                             rand_num = np.random.uniform()
-                            if rand_num > VAL_SPLIT:
+                            if rand_num > 0.2:
                                 new_path = os.path.join(BASE_DIR,'train')
                                 same_cat_count_train+=1
-                            else:
+                            elif rand_num > 0.1 and rand_num <= 0.2:
                                 new_path = os.path.join(BASE_DIR,'val')
                                 same_cat_count_val+=1
+                            else:
+                                new_path = os.path.join(BASE_DIR,'test')
+                                same_cat_count_test+=1
                             same_cat_count+=1
                             new_path = os.path.join(new_path,dir)
                             if not os.path.exists(new_path):
@@ -42,10 +58,16 @@ if __name__ == "__main__":
                         total_count+=1
             cat_count_train_list.append(same_cat_count_train)
             cat_count_val_list.append(same_cat_count_val)
+            cat_count_test_list.append(same_cat_count_test)
             cat_count_total_list.append(same_cat_count)
     print(cat_count_train_list)
+    print("total_train_size: %s"%sum(cat_count_train_list))
     print(cat_count_val_list)
+    print("total_val_size: %s"%sum(cat_count_val_list))
+    print(cat_count_test_list)
+    print("total_test_size: %s"%sum(cat_count_test_list))
     print(cat_count_total_list)
+    print("total_data_size: %s"%sum(cat_count_total_list))
     print(total_count)
     cmd_train_l = ["[%s]*%s"%(i,count) for i,count in enumerate(cat_count_train_list)]
     cmd_train = '+'.join(cmd_train_l)
@@ -54,6 +76,7 @@ if __name__ == "__main__":
     cmd_val_l = ["[%s]*%s"%(i,count) for i,count in enumerate(cat_count_val_list)]
     cmd_val = '+'.join(cmd_val_l)
     cmd_val = 'val_labels = np.array('+cmd_val+')'
+
 
     print(cmd_train)
     print(cmd_val)

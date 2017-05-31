4d9f903e1b6d30173a45b8c847e0b39d0df51b50
danielwhsu <daniel.hsu7@gmail.com>
2017-05-31 04:24:42 +0000

    classifier 3 done

--- keras_art/code/classifier_from_little_data_script_3.py
+++ keras_art/code/classifier_from_little_data_script_3.py
@@ -45,15 +45,15 @@ from keras.models import Model
 
 # path to the model weights files.
 weights_path = '../keras/examples/vgg16_weights.h5'
-top_model_weights_path = 'fc_model.h5'
+top_model_weights_path = 'bottleneck_fc_model.h5'
 # dimensions of our images.
 img_width, img_height = 150, 150
 
-train_data_dir = 'data/train'
-validation_data_dir = 'data/validation'
-nb_train_samples = 2000
-nb_validation_samples = 800
-epochs = 1
+train_data_dir = '../data/Pandora_tiny_restructured'
+validation_data_dir = '../data/Pandora_tiny_restructured'
+nb_train_samples = 224
+nb_validation_samples = 224
+epochs = 2
 batch_size = 16
 
 
@@ -66,7 +66,7 @@ top_model = Sequential()
 top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
 top_model.add(Dense(256, activation='relu'))
 top_model.add(Dropout(0.5))
-top_model.add(Dense(1, activation='sigmoid'))
+top_model.add(Dense(3, activation='sigmoid'))
 
 
 # note that it is necessary to start with a fully-trained
@@ -86,7 +86,7 @@ for layer in model.layers[:25]:
 
 # compile the model with a SGD/momentum optimizer
 # and a very slow learning rate.
-model.compile(loss='binary_crossentropy',
+model.compile(loss='categorical_crossentropy',
               optimizer=optimizers.SGD(lr=1e-4, momentum=0.9),
               metrics=['accuracy'])
 
@@ -103,13 +103,13 @@ train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
     validation_data_dir,
     target_size=(img_height, img_width),
     batch_size=batch_size,
-    class_mode='binary')
+    class_mode='categorical')
 
 # fine-tune the model
 model.fit_generator(
